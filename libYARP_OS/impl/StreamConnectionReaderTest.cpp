054c554e387250ca713534b99ef9f51465b3a63d
Daniele E. Domenichelli <daniele.domenichelli@iit.it>
2018-10-04 19:25:33 +0200

    StreamConnectionReaderTest.cpp

---
+++ libYARP_OS/impl/StreamConnectionReaderTest.cpp
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) 2006-2018 Istituto Italiano di Tecnologia (IIT)
+ * Copyright (C) 2006-2010 RobotCub Consortium
+ * All rights reserved.
+ *
+ * This software may be modified and distributed under the terms of the
+ * BSD-3-Clause license. See the accompanying LICENSE file for details.
+ */
+
+#include <yarp/os/impl/StreamConnectionReader.h>
+#include <yarp/os/StringInputStream.h>
+#include <yarp/os/StringOutputStream.h>
+
+#if defined(USE_SYSTEM_CATCH)
+#include <catch.hpp>
+#else
+#include "catch.hpp"
+#endif
+
+using namespace yarp::os;
+using namespace yarp::os::impl;
+
+TEST_CASE("OS::StreamConnectionReaderTest", "[yarp::os]") {
+
+    SECTION("testing reading") {
+        StringInputStream sis;
+        StringOutputStream sos;
+        sis.add("Hello\ngood evening and welcome");
+        StreamConnectionReader sbr;
+        Route route;
+        sbr.reset(sis,nullptr,route,10,true);
+        std::string line = sbr.expectLine();
+        CHECK(line == "Hello"); // one line
+    }
+}
